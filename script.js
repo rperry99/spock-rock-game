e36fefcaffba2a9c6ec88f8391ecb88fb93e1f19
Russ <rperry1886@gmail.com>
2020-08-23 13:20:05 -0400

    Add computer chouce logic

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+let computerChoice = '';
+
 // Reset all selected icons
 function resetSelected() {
   allGameIcons.forEach((icon) => {
@@ -31,10 +33,62 @@ function resetSelected() {
   });
 }
 
+// Random computer choice
+function computerRandomChoice() {
+  const computerChoiceNumber = Math.floor(Math.random() * 5);
+  if (computerChoiceNumber === 0) {
+    computerChoice = 'rock';
+  } else if (computerChoiceNumber === 1) {
+    computerChoice = 'paper';
+  } else if (computerChoiceNumber === 2) {
+    computerChoice = 'scissors';
+  } else if (computerChoiceNumber === 3) {
+    computerChoice = 'lizard';
+  } else {
+    computerChoice = 'spock';
+  }
+}
+
+// Add selected stying and computer choice
+function displayComputerChoice() {
+  // Add Selected styling and update player choice
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      computerPaper.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+      computerScissors.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissors';
+      break;
+    case 'lizard':
+      computerLizard.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Lizard';
+      break;
+    case 'spock':
+      computerSpock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Spock';
+      break;
+    default:
+      break;
+  }
+}
+
+// Call functions to process turn
+function checkResult() {
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+}
+
 // Passing player selection valie and style icons
 function select(playerChoice) {
   // Reset all icon selectiosn
-  resetSelected();
+  checkResult();
 
   // Add Selected styling and update player choice
   switch (playerChoice) {
